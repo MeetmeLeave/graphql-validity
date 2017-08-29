a3c49b5ffacce1c348bf1db91b98c528dd5d9164
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2017-08-29 16:41:21 +0300

    - very first package version - work in progress

---
+++ src/schema-wrapper.ts
@@ -0,0 +1,78 @@
+import {
+    GraphQLObjectType,
+    GraphQLSchema,
+} from 'graphql';
+
+export const Processed = Symbol();
+export const FieldValidationDefinitions: any = {};
+
+const validationResults: any[] = [];
+
+export function wrapExtension({ result }: any) {
+    result.errors = validationResults.map(error => {
+        return {
+            message: error.message
+        };
+    });
+    return null;
+}
+
+export function wrapResolvers(entity: any) {
+    if (entity.constructor.name === 'GraphQLSchema') {
+        wrapSchema(entity);
+    } else if (entity.constructor.name === 'GraphQLObjectType') {
+        wrapType(entity);
+    } else {
+        wrapField(entity);
+    }
+}
+
+function wrapField(field: any) {
+    const resolve = field.resolve;
+    if (field[Processed] || !resolve) {
+        return;
+    }
+
+    field[Processed] = true;
+    field.resolve = async function (...args: any[]) {
+        try {
+            if (validationResults) {
+                let validators = FieldValidationDefinitions[field.type] || [];
+                for (let validator of validators) {
+                    Array.prototype.push.apply(validationResults, await validator.call(this, ...args));
+                }
+            }
+
+            return await resolve.call(this, ...args);
+        } catch (e) {
+            console.error('err', e);
+            throw e;
+        }
+    };
+}
+
+function wrapType(type: any) {
+    if (type[Processed] || !type.getFields) {
+        return;
+    }
+
+    const fields = type.getFields();
+    for (const fieldName in fields) {
+        if (!Object.hasOwnProperty.call(fields, fieldName)) {
+            continue;
+        }
+
+        wrapField(fields[fieldName]);
+    }
+}
+
+function wrapSchema(schema: any) {
+    const types = schema.getTypeMap();
+    for (const typeName in types) {
+        if (!Object.hasOwnProperty.call(types, typeName)) {
+            continue;
+        }
+
+        wrapType(types[typeName]);
+    }
+}
