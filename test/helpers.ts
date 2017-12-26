acb9d89c8d39c35dc83ba7c7c7cade6587943fbb
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2017-12-26 13:39:20 +0200

    - helpers tests

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,11 +1,26 @@
-import * as mock from 'mock-require';
 import { expect } from 'chai';
-import * as sinon from 'sinon';
 
 import {
-    onUnhandledError
+    onUnhandledError,
+    ValidityError
 } from '../src/helpers';
 
 describe('helpers', () => {
+    describe('onUnhandledError', () => {
+        it('Should mask error if it was not instantiated from ValidityError', () => {
+            const errorMessage = 'test error message';
+            const result = onUnhandledError(new Error(errorMessage));
+            expect(result.message).to.not.have.string(errorMessage);
+            expect(result.message).to.have.string('An internal error occured');
+        });
 
+        it('Should not mask error if it was not instantiated from ValidityError', () => {
+            const errorMessage = 'test error message';
+            const error = new ValidityError(errorMessage)
+            const result = onUnhandledError(error);
+
+            expect(result.message).to.have.string(errorMessage);
+            expect(result.message).to.not.have.string('An internal error occured');
+        });
+    });
 });
