627ab367a041a6fec1f57c350945bf775a0aca31
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2017-12-28 02:13:06 +0200

    - schema wrapper tests (not complete)

--- test/schema-wrapper.ts
+++ test/schema-wrapper.ts
@@ -4,11 +4,105 @@ import * as sinon from 'sinon';
 
 import {
     wrapResolvers,
+    Processed,
     graphQLValidityHapiMiddleware,
     graphQLValidityExpressMiddleware,
     graphQLValidityKoaMiddleware
 } from '../src/schema-wrapper';
+import { ValidityConfig } from "../src/helpers";
 
 describe('schema-wrapper', () => {
+    describe('wrapResolvers', () => {
+        it('should create proxy for field resolver', () => {
+            const resolve = function () {
+                return true;
+            };
+            const field = { resolve };
+            wrapResolvers(field);
 
+            expect(field.resolve).to.not.equal(resolve);
+        });
+
+        it('should create proxy for field resolvers on a given object type', () => {
+            const resolve = function () {
+                return true;
+            };
+            const field = { resolve };
+
+            const type = new GraphQLObjectType([field], 'Test');
+
+            wrapResolvers(type);
+
+            expect(field.resolve).to.not.equal(resolve);
+        });
+
+        it('should create proxy for field resolvers on a given schema', () => {
+            const resolve = function () {
+                return true;
+            };
+            const field = { resolve };
+
+            const type = new GraphQLObjectType([field], 'Test');
+            const typesMap = {
+                'Test': type
+            };
+            const schema = new GraphQLSchema(typesMap);
+            wrapResolvers(schema);
+
+            expect(field.resolve).to.not.equal(resolve);
+        });
+
+        it('should set Processed to true', () => {
+            const resolve = function () {
+                return true;
+            };
+            const field = { resolve };
+            wrapResolvers(field);
+
+            expect(field[Processed]).to.be.true;
+        });
+
+        it('resolve should return result if validity is not set up', async () => {
+            const resolve = function () {
+                return true;
+            };
+            const field = { resolve };
+            wrapResolvers(field);
+
+            const result = await field.resolve();
+            expect(result).to.be.true;
+        });
+
+        it('resolve should return result if validity is set up', async () => {
+            const resolve = function () {
+                return true;
+            };
+            const field = { resolve };
+            wrapResolvers(field);
+
+            const validity = {};
+
+            const result = await field.resolve();
+            expect(result).to.be.true;
+        });
+    });
 });
+
+class GraphQLObjectType {
+    constructor(private fields: any[], public name: string) {
+    }
+
+    getFields() {
+        return this.fields;
+    }
+}
+
+class GraphQLSchema {
+    constructor(private types: any) {
+
+    }
+
+    getTypeMap() {
+        return this.types;
+    }
+}
