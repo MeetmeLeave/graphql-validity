bdb2223e1bf9ec747227a958f190c49a34664391
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2018-05-23 16:20:54 +0300

    - fix for not processing errors in validation

--- src/validation.ts
+++ src/validation.ts
@@ -47,7 +47,7 @@ export function getResponseValidationResults(validity: any, data: any) {
                 validity.___validationResults.map(
                     error => {
                         return {
-                            message: processError(error, validity.config)
+                            message: processError(error, validity.config).message
                         };
                     })
             );
