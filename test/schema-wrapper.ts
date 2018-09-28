867d7cbf7bf20d9a7e8af914cec04400d0ccf55b
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2018-09-28 18:01:05 +0300

    - updated schema-wrapper tests

--- test/schema-wrapper.ts
+++ test/schema-wrapper.ts
@@ -9,6 +9,7 @@ import {
     graphQLValidityKoaMiddleware
 } from '../lib/schema-wrapper';
 import { ValidityConfig } from "../lib/helpers";
+import { FieldValidationDefinitions } from "../lib";
 
 describe('schema-wrapper', () => {
     describe('wrapResolvers', () => {
@@ -73,11 +74,63 @@ describe('schema-wrapper', () => {
         });
 
         it('resolve should return result if validity is set up', async () => {
+            FieldValidationDefinitions['$'] = [() => {return new Error('test1')}];
+            const resolve = function (...args: any[]) {
+                return true;
+            };
+            const field = { resolve, name: 'Test' };
 
+            const type = new GraphQLObjectType([field], 'Test');
+            const typesMap = {
+                'Test': type
+            };
+            const schema = new GraphQLSchema(typesMap);
+            wrapResolvers(schema);
+
+            const validity = {
+                ___globalValidationResultsCaptured: false,
+                ___validationResults: [new Error('test2'), new Error('test1')]
+            };
+
+            const result = await field.resolve(
+                {
+                    parentType: 'Test',
+                    rootValue: {
+                        __graphQLValidity: validity
+                    }
+                });
+            expect(result).to.be.true;
         });
 
         it('resolve should perform validation if validity is set up', async () => {
+            FieldValidationDefinitions['$'] = [() => {return new Error('test1')}];
+            const resolve = function (...args: any[]) {
+                return true;
+            };
+            const field = { resolve, name: 'Test' };
 
+            const type = new GraphQLObjectType([field], 'Test');
+            const typesMap = {
+                'Test': type
+            };
+            const schema = new GraphQLSchema(typesMap);
+            wrapResolvers(schema);
+
+            const validity = {
+                ___validationResults: []
+            };
+
+            await field.resolve(
+                {
+                    parentType: 'Test',
+                    rootValue: {
+                        __graphQLValidity: validity
+                    }
+                });
+
+            expect(validity.___validationResults).to.not.be.null;
+            expect(validity.___validationResults.length).to.equal(1);
+            expect(validity.___validationResults[0].message).to.equal('test1');
         });
     });
 });
